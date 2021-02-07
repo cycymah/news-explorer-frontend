5ddf5a571d41a76d5dc272ea90a16bbc4d6aeef5
Ivan Solnyshkin <ethosy@yandex.ru>
2021-02-08 00:38:36 +0700

    fix: review fix scroll, validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,6 +1,22 @@
+import classNames from 'classnames';
+import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+
 import './SearchForm.css';
+import { searchValidationConfig } from '../../constants/form';
 
 function SearchForm() {
+  const [isDisabledBtn, setButtonDisabled] = useState(true);
+  const { register, errors } = useForm({ mode: 'onChange' });
+  const validation = register(searchValidationConfig);
+
+  const buttonValidityClass = classNames('search__btn', {
+    search__btn_inactive: isDisabledBtn,
+  });
+
+  const onChangeValidation = () =>
+    errors.search ? setButtonDisabled(true) : setButtonDisabled(false);
+
   return (
     <section className="search">
       <h1 className="search__title">Что творится в мире?</h1>
@@ -15,8 +31,14 @@ function SearchForm() {
           className="search__input"
           placeholder="Введите тему новости"
           autoComplete="off"
+          ref={validation}
+          onChange={onChangeValidation}
         />
-        <button type="submit" className="search__btn">
+        <button
+          type="submit"
+          className={buttonValidityClass}
+          disabled={isDisabledBtn}
+        >
           Искать
         </button>
       </form>
